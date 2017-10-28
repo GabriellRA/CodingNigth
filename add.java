73292cc7f266d3a55b4b20f05ed209bdc2708c14
Marcelo Regra da Silva <marregsil97@gmail.com>
2017-10-28 23:28:13 +0100

    added

---
+++ add.java
@@ -0,0 +1,38 @@
+
+public class Operations{
+
+import java.util.Scanner;
+
+public int add (int x, int y) {
+return x + y; }
+
+public int sub (int x, int y) {
+return x - y; }
+
+public int mul (int x, int y) {
+return x * y; }
+
+public int div (int x, int y) {
+return x / y; }
+
+public int mod (int x, int y) {
+return x%y; }
+
+public int lessThen (int x, int y) {
+	if (x>y) {
+		return y;
+		
+}else{return x} }
+	
+	
+public int greaterThen (int x, int y) {
+	if (x<y) {
+		return y;
+		
+}else{return x} }
+
+public int neg(int x)  {
+	return -x;} 
+
+
+}
